a671bf4d434772bcc9508edda5ee44a4e9d299c1
20161104607 <a20161104607@mac26.local>
2018-06-27 10:00:31 +0800

    打分
    1～10

--- main.cpp
+++ main.cpp
@@ -6,64 +6,89 @@
 //  Copyright © 2018年 20161104607. All rights reserved.
 //
 
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
-struct Student{
-    string name;
-    int score[7];
-    int sum;
-};
-int main(){
-    //评委评分 最高最低 取平均分
-    int n;
-    cin>>n;
-    Student stu[n];
-    for(int i=0;i<n;i++){
-        cin>>stu[i].name;
-        for(int j=0;j<7;j++){
-            cin>>stu[i].score[j];
-        }
-        int max=stu[i].score[0],min=stu[i].score[0];
-        for(int j=0;j<7;j++){
-            if(stu[i].score[j]>max)max=stu[i].score[j];
-            if(stu[i].score[j]<min)min=stu[i].score[j];
-        }
-        for(int j=0;j<7;j++){
-            stu[i].sum+=stu[i].score[j];
-        }
-        stu[i].sum=stu[i].sum-max-min;
-        cout<<"sum="<<stu[i].sum<<endl;
+int max (int x,int y);
+int min (int x,int y);
+int main()
+{
+    int a=0,b=101,c=0,i,j;
+    double average;
+    cout<<"请输入选手成绩"<<endl;
+    for(i=1;i<=10;i++)
+    {
+        cout<<"第"<<i<<"位评委给分：";
+        cin>>j;
+        a=max(a,j);
+        b=min(b,j);
+        c=c+j;
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=i;j<n;j++){
-            if(stu[i].sum<stu[j].sum){
-                Student temp;
-                temp=stu[i];
-                stu[i]=stu[j];
-                stu[j]=temp;
-            }
-        }
-    }
-    for(int i=0;i<n;i++){
-        cout<<stu[i].name<<' '<<stu[i].sum<<endl;
-    }
-    
-    
-    //读取.csv文件
-
-    string value;
-    char filename[256];
-    cout<<"请输入文件名"<<endl;
-    cin>>filename;
-    ifstream infile(filename);
-    while(infile.good()){
-        //.csv文件用","作为分隔符
-        getline(infile,value,',');
-        cout<<value<<endl;  
-    }
-    
-    
+    average=(c-a-b)/8;
+    cout<<"减去一个最高分"<<a<<","<<"减去一个最低分"<<b<<endl;
+    cout<<"该选手的最后得分："<<average<<endl;
     return 0;
 }
+
+int max(int x, int y)
+{
+    if(x>y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+
+int min(int x, int y)
+{
+    if(x>y)
+    {
+        return y;
+    }
+    else
+    {
+        return x;
+    }
+    return 0;
+}
+
+    
+
+    void split(string str,string separator,vector<string> &result)
+    {//对每一行以逗号为分隔符进行分割
+        result.clear();
+        int cutAt;
+        while( (cutAt = str.find_first_of(separator)) != str.npos )
+        {
+            if(cutAt > 0)
+            {
+                result.push_back(str.substr(0, cutAt));
+            }
+            str = str.substr(cutAt + 1);
+        }
+        if(str.length() > 0)
+        {
+            result.push_back(str);
+        }
+    }
+
+    ifstream file ( "./template/GPLT.csv" );
+    string row;
+    getline(file,row);//剔除首部
+    vector<string> infRow;
+    int typeChoose;
+    while ( file.good() )
+    {
+        getline(file,row);
+        split(row,",",infRow);
+        if(infRow.size())//有效的数据
+        {
+            //doSomething
+        }
+     }
